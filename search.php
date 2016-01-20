557b2b682a9fe978a67f30fd241d55cb18262ca1
Archit Bakliwal <architbakliwal@gmail.com>
2016-01-20 20:34:00 +0530

    distance integration

--- search.php
+++ search.php
@@ -3,11 +3,33 @@ include dirname( __FILE__ ).'/config.php';
 
 $json_response = array();
 
+function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+
+	$theta = $lon1 - $lon2;
+	$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+	$dist = acos($dist);
+	$dist = rad2deg($dist);
+	$miles = $dist * 60 * 1.1515;
+	$unit = strtoupper($unit);
+
+	if ($unit == "K") {
+		return ($miles * 1.609344);
+	} else if ($unit == "M") {
+		return ($miles * 1609.344);
+	} else {
+		return $miles;
+	}
+}
+
 $type = strip_tags( trim( $_POST["searchType"] ) );
 $value = strip_tags( trim( $_POST["searchVal"] ) );
+$lat = strip_tags( trim( $_POST["latitude"] ) );
+$lng = strip_tags( trim( $_POST["longitude"] ) );
 
 $finaltype = htmlspecialchars( $type, ENT_QUOTES, 'UTF-8' );
 $finalvalue = htmlspecialchars( $value, ENT_QUOTES, 'UTF-8' );
+$finallat = htmlspecialchars( $lat, ENT_QUOTES, 'UTF-8' );
+$finallng = htmlspecialchars( $lng, ENT_QUOTES, 'UTF-8' );
 
 if($finaltype == "all") {
   $sqlsearch = "SELECT * FROM `teacher` WHERE MATCH(skill,stream,board,class,subject) AGAINST('" . $finalvalue . "') AND is_active = 'Y'";
@@ -25,8 +47,19 @@ while ($row = mysql_fetch_array($selectsearch, MYSQL_ASSOC)) {
   $row_array['uid'] = $row['uid'];
   $row_array['name'] = $row['name'];
   $row_array['area'] = $row['area'];
+  $row_array['latitude'] = $row['latitude'];
+  $row_array['longitude'] = $row['longitude'];
+  if(isset($finallat) && isset($finallng)) {
+  	$row_array['distance'] = distance($row_array['latitude'], $row_array['longitude'], $finallat, $finallng, "M");
+  }
   //push the values in the array
-  array_push($json_response,$row_array);
+  array_push($json_response, $row_array);
+}
+
+if(isset($finallat) && isset($finallng)) {
+	usort($json_response, function($a, $b) {
+	    return $a['distance'] - $b['distance'];
+	});
 }
 
 echo json_encode($json_response);
